e9961cebb6ff05c1d85473b14c52a2b1455d17a7
asherikov <asherikov@yandex.com>
2014-09-15 19:30:54 +0400

    Minor changes in building process of matlab/octave interface.
    - New targets in Makefile.
    - Separate compilation scripts for Matlab and Octave.

--- interfaces/matlab-octave/make-octave.m
+++ interfaces/matlab-octave/make-octave.m
@@ -3,12 +3,10 @@
 %
 % http://stackoverflow.com/questions/15568925/mex-files-on-mountain-lion-explicit-instantiation-error
 
-system('rm -rf *.mexmaci*');
 
 % ------------------------------------------------------------
 % compilation settings
 % ------------------------------------------------------------
-CXX           = 'g++';
 EIGEN_INCLUDE = '/usr/local/include/eigen3';
 LEXLS_INCLUDE = '../../include';
 
@@ -21,7 +19,6 @@ FILE{2}       = 'lexlsi.cpp';
 INCLUDES = ['-I', EIGEN_INCLUDE, ' -I', LEXLS_INCLUDE];
 
 for i=1:length(FILE)
-%  cc = ['mex ', 'CXX=', CXX, ' ', INCLUDES, ' ', FILE{i}];
   cc = ['mex -v', ' ', INCLUDES, ' ', FILE{i}];
 
   disp(cc);
